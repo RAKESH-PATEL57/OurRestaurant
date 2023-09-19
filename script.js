e1cb6c0a9764ad16eb6aa72726001449a3eb2c48
Rakesh Patel <107678438+RAKESH-PATEL57@users.noreply.github.com>
2023-09-19 17:27:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ var swiper = new Swiper(".home-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 1000000
+      delay: 1000000,
       disableOnInteraction: false,
     },
     pagination: {
