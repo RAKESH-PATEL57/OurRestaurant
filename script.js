3562bd06002a9c0468594e25742965ffa1ab8584
Chiranjeeb Das <chiranjeebdas2003@gmail.com>
2023-09-19 17:33:32 +0530

    Update script.js
    -"Add to cart" adds multiple items
    -Cart fix

--- script.js
+++ script.js
@@ -264,6 +264,9 @@ function addToCard(key)
     if(listCards[key] == null){
         listCards[key] = products[key];
         listCards[key].quantity = 1;
+     }
+    else{
+        listCards[key].quantity += 1;
     }
     reloadCard();
 }
@@ -273,18 +276,19 @@ function reloadCard(){
     let count = 0;
     let totalPrice = 0;
     listCards.forEach((value, key) => {
-        totalPrice = totalPrice + value.price;
+        currentPrice = value.newprice!=null ? value.newprice : value.price;
+        totalPrice = totalPrice + currentPrice*value.quantity;
         count = count + value.quantity;
         if(value != null){
             let newDiv = document.createElement('li');
             newDiv.innerHTML = `
                 <div><img src="./images/${value.image}"/></div>
                 <div>${value.name}</div>
-                <div>${value.price.toLocaleString()}</div>
+                <div>${currentPrice.toLocaleString()}</div>
                 <div>
-                    <button onclick = "changeQuantity(${key}, ${value.quantity - 1})" >-</button>
+                    <button onclick = "changeQuantity(${key}, ${value.quantity - 1},${currentPrice})" >-</button>
                     <div class="count">${value.quantity}</div>
-                    <button onclick = "changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                    <button onclick = "changeQuantity(${key}, ${value.quantity + 1},${currentPrice})">+</button>
                     
                 </div>
                 
@@ -299,11 +303,11 @@ function reloadCard(){
             newDiv.innerHTML = `
                 <div><img src="./images/${value.image}"/></div>
                 <div>${value.name}</div>
-                <div>${value.price.toLocaleString()}</div>
+                <div>${currentPrice.toLocaleString()}</div>
                 <div>
-                    <button onclick = "changeQuantity(${key}, ${value.quantity - 1})" >-</button>
+                    <button onclick = "changeQuantity(${key}, ${value.quantity - 1},${currentPrice})" >-</button>
                     <div class="count">${value.quantity}</div>
-                    <button onclick = "changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                    <button onclick = "changeQuantity(${key}, ${value.quantity + 1},${currentPrice})">+</button>
                     
                 </div>
                 
@@ -315,7 +319,7 @@ function reloadCard(){
     quantity.innerText = count;
 }
 
-function changeQuantity(key, quantity){
+function changeQuantity(key, quantity,price){
     if(quantity == 0)
     {
         delete listCards[key];
@@ -323,11 +327,8 @@ function changeQuantity(key, quantity){
     else
     {
         listCards[key].quantity = quantity;
-        listCards[key].price = quantity * products[key].price;
+        listCards[key].price=price;
     }
     
-    // let timeSave = "expires=Thus, 31 Dec 2025 23:59:59 UTC";
-    // document.cookie = "listCart= "+ JSON.stringify(listCard)+"; "+timeSave+";path/;";
     reloadCard();
 }
-
